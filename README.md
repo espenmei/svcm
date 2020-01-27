f47d28c27cbccb7fa891a9563b2e51f5ef6fa088
Espen Moen Eilertsen <espeneil@outlook.com>
2020-01-27 22:13:51 +0100

    Readme work

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-**svcmr** is intended for specifying models combining structural
-equation models with sparse relationship matrices. A relationship matrix
-may for example encode the genetic relatedness among individuals or
-different forms of shared environments. The interface is is inspired by
-**OpenMx**, but uses the **Matrix** and **sparseMVN** R-packages for
-computation.
-
-### Example code
+**svcmr** is intended for easy specification of models combining
+structural equation models with sparse relationship matrices. A
+relationship matrix may for example encode the genetic relatedness among
+individuals or different forms of shared environments. The interface is
+inspired by **OpenMx**, but uses the **Matrix** and **sparseMVN**
+R-packages for computation.
--- Test/CheckCode.R
+++ Test/CheckCode.R
@@ -1,8 +1,6 @@
 library(svcmr)
 library(lavaan)
 
-set.seed(080318)
-
 N = 100
 P = 4
 Y = matrix(rnorm(N * P), N, P)
@@ -11,14 +9,16 @@ W = matrix(rnorm(N * 2), N, 2)
 R = Matrix::Diagonal(N)
 
 # Common factors
-l = pm(4, 1, paste0("l", 1:4), c(F, T, T, T), 1, "l")
+l = pm(4, 1, paste0("l", c(2, 2, 2, 2)), c(T, T, T, T), 1, "l")
 c = pm(1, 1, "c", T, 1, "c")
 # Specific factors
-s = pm(4, 4, paste0("es", 1:16), free = diag(T, 4), values = c(diag(2, 4)), name = "s")
+lab_es = matrix("e", 4, 4)
+#diag(lab_es) = "e"
+s = pm(4, 4, lab_es, free = diag(T, 4), values = c(diag(2, 4)), name = "s")
 # VCs
 svc1 = svc(l %*% (c %*% t(c)) %*% t(l) + s %*% t(s), R = R)
 # Means
-B = pm(4, 1, labels = paste0("b", 1:4), free = T, values = colMeans(Y), name = "B")
+B = pm(4, 1, labels = c("b1", "l2", "l2", "b4"), free = T, values = colMeans(Y), name = "B")
 mc_X = mc(B, X = X)
 A = pm(1, 2, labels = paste0("a", 1:2), free = T, values = 0, name = "A")
 mc_W = mc(l %*% A, X = W)
@@ -26,16 +26,13 @@ mc_W = mc(l %*% A, X = W)
 mod1 = svcm(Y, l, c, s, svc1, B, mc_X, A, mc_W)
 fit1 = fitm(mod1, se = T, control = list(trace = 6))
 summary(fit1)
-fit1$time
+
+as.vector(Matrix::t(B$values %*% t(X)))
 # Compute
 # --------------------------
 compute(mod1, l %*% (c %*% t(c)) %*% t(l))
 compute(fit1, l %*% (c %*% t(c)) %*% t(l))
-anova(fit1)
 
-logLik(fit1)
-AIC(fit1)
-BIC(fit1)
 # Comparison lavaan
 # --------------------------
 colnames(Y) = c("v1", "v2", "v3", "v4")
@@ -48,8 +45,3 @@ c~ a1*w1 + a2*w2
 fit_lav = cfa(mod_lav, dat = dat_lav, meanstructure = T)
 summary(fit_lav)
 
-logLik(fit1)
-logLik(fit_lav)
-
-vcov(fit_lav)
-round(vcov(fit1), 3)
